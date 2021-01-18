079d7bb4422e2157fb4741e16a8333dd06f4c0f5
pplociennik <pplociennik097@gmail.com>
2021-01-19 00:58:31 +0100

    Fixed project structure

--- passnail.core/src/main/java/com/passnail/server/core/main/ServerStarter.java
+++ passnail.core/src/main/java/com/passnail/server/core/main/ServerStarter.java
@@ -2,8 +2,10 @@ package com.passnail.server.core.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ServerStarter {
 
     public static void main(String[] args) {
