a803d29b773e834bcadbfa1a8446cd1f2d483dd8
pplociennik <pplociennik097@gmail.com>
2021-03-11 20:42:58 +0100

    Fixed in structure

--- passnail.server.core/src/main/java/com/passnail/server/core/app/config/AppConfig.java
+++ passnail.server.core/src/main/java/com/passnail/server/core/app/config/AppConfig.java
@@ -1,7 +1,7 @@
 package com.passnail.server.core.app.config;
 
-import com.passnail.data.DataServiceModuleConfig;
 import com.passnail.data.model.DataModelModuleConfig;
+import com.passnail.server.data.DataServiceModuleConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
