a6597bb6632c4e81efcca436426d31e0343ad012
pplociennik <pplociennik097@gmail.com>
2021-03-12 16:51:04 +0100

    In progress

--- passnail.server.core/src/main/java/com/passnail/server/core/app/config/WebSecurityConfiguration.java
+++ passnail.server.core/src/main/java/com/passnail/server/core/app/config/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.passnail.security.config;
+package com.passnail.server.core.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
