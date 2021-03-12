027bcc27109555f6c9af714d334afece89a1c7e3
pplociennik <pplociennik097@gmail.com>
2021-03-12 18:17:59 +0100

    Changed security settings (fixed)

--- passnail.server.core/src/main/java/com/passnail/server/core/app/service/impl/UserService.java
+++ passnail.server.core/src/main/java/com/passnail/server/core/app/service/impl/UserService.java
@@ -6,7 +6,7 @@ import com.passnail.server.core.app.entity.UserEntity;
 import com.passnail.server.core.app.repository.UserRepository;
 import com.passnail.server.core.app.service.UserServiceIf;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -24,8 +24,8 @@ public class UserService implements UserServiceIf {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private BCryptPasswordEncoder encoder;
+//    @Autowired
+//    private BCryptPasswordEncoder encoder;
 
 
     @Override
