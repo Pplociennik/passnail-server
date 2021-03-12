b2a1d29c13700186950958f5bf3a35673945c3e5
pplociennik <pplociennik097@gmail.com>
2021-03-12 18:55:07 +0100

    Added RequestBody annotations

--- passnail.server.core/src/main/java/com/passnail/server/core/app/controller/SynchronizationController.java
+++ passnail.server.core/src/main/java/com/passnail/server/core/app/controller/SynchronizationController.java
@@ -6,6 +6,7 @@ import com.passnail.server.core.app.dto.UserDto;
 import com.passnail.server.core.app.service.SynchronizationServiceIf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +20,13 @@ public class SynchronizationController {
 
 
     @RequestMapping(method = RequestMethod.POST, value = "/user/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String createAnOnlineUser(UserDto aUserDto) {
+    public String createAnOnlineUser(@RequestBody UserDto aUserDto) {
         return synchronizationService.createOnlineUserAndReturnOnlineId(aUserDto);
     }
 
 
     @RequestMapping(method = RequestMethod.POST, value = "/user/synchronize", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public SynchronizationResultDto synchronizeUser(UserDto aUserFromClient) {
+    public SynchronizationResultDto synchronizeUser(@RequestBody UserDto aUserFromClient) {
         return synchronizationService.synchronizeServer(aUserFromClient);
     }
 
