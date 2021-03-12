84983e6eebab3a174e9bf86430e2a745fc471aca
pplociennik <pplociennik097@gmail.com>
2021-03-12 18:46:01 +0100

    Changed consumes and produces

--- passnail.server.core/src/main/java/com/passnail/server/core/app/controller/SynchronizationController.java
+++ passnail.server.core/src/main/java/com/passnail/server/core/app/controller/SynchronizationController.java
@@ -5,6 +5,7 @@ import com.passnail.server.core.app.dto.SynchronizationResultDto;
 import com.passnail.server.core.app.dto.UserDto;
 import com.passnail.server.core.app.service.SynchronizationServiceIf;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,13 @@ public class SynchronizationController {
     private SynchronizationServiceIf synchronizationService;
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "/user/create")
+    @RequestMapping(method = RequestMethod.POST, value = "/user/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String createAnOnlineUser(UserDto aUserDto) {
         return synchronizationService.createOnlineUserAndReturnOnlineId(aUserDto);
     }
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "/user/synchronize")
+    @RequestMapping(method = RequestMethod.POST, value = "/user/synchronize", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public SynchronizationResultDto synchronizeUser(UserDto aUserFromClient) {
         return synchronizationService.synchronizeServer(aUserFromClient);
     }
