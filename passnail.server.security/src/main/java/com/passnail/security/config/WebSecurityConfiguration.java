de988d04d81a9ef84e10e50028a93880e2ac952b
pplociennik <pplociennik097@gmail.com>
2021-03-12 16:22:21 +0100

    Authenticated all requests

--- passnail.server.security/src/main/java/com/passnail/security/config/WebSecurityConfiguration.java
+++ passnail.server.security/src/main/java/com/passnail/security/config/WebSecurityConfiguration.java
@@ -21,7 +21,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .anyRequest().authenticated();
 
     }
 }
