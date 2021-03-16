d8054564edf83b7a72627af1938b31e9099e9c10
pplociennik <pplociennik097@gmail.com>
2021-03-16 22:35:44 +0100

    modified equals method in CredentialsEntity

--- passnail.server.core/src/main/java/com/passnail/server/core/app/entity/CredentialsEntity.java
+++ passnail.server.core/src/main/java/com/passnail/server/core/app/entity/CredentialsEntity.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
 import java.util.UUID;
@@ -100,6 +102,7 @@ public class CredentialsEntity {
 
     @Override
     public boolean equals(Object o) {
+        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CredentialsEntity that = (CredentialsEntity) o;
@@ -108,7 +111,7 @@ public class CredentialsEntity {
                 Objects.equals(login, that.login) &&
                 Objects.equals(url, that.url) &&
                 Objects.equals(description, that.description) &&
-                Objects.equals(creationDate, that.creationDate);
+                Objects.equals(df.format(creationDate), df.format(that.creationDate));
     }
 
     @Override
